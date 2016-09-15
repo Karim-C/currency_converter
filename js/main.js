b668c5437c757987b8740230d67e088b297cf6de
Karim <kcis880@aucklanduni.ac.nz>
2016-09-16 11:55:54 +1200

    finished

--- js/main.js
+++ js/main.js
@@ -1,68 +1,53 @@
-// Get jquery objects from DOM
-var imgSelector = $("#my-file-selector");
-var refreshbtn = $("#refreshbtn");
+var age; // age of user
+var imgSelector = $("#my-file-selector")[0];
+// listener for the upload picture button
+imgSelector.addEventListener("change", function () {
+    pageheader.innerHTML = "Analysing your face now";
+    processImage(function (file) {
+        sendEmotionRequest(file, function (faceAttributes) {
+            // extract age from recieved data
+            age = getAge(faceAttributes);
+            changeUI();
+        });
+    });
+});
 var pageheader = $("#page-header")[0];
 var pagecontainer = $("#page-container")[0];
-
-refreshbtn.on("click", function () {
-    // Load random song based on mood
-    alert("You clicked the button"); //can demo with sweetAlert plugin
-});
-
-// Manipulate the DOM
+var refreshbtn = $("#refreshbtn")[0];
 function changeUI() {
-    //Show detected mood
-    pageheader.html("Your mood is: ...");
-
-    //Display song refresh button
-    refreshbtn.css("display", "inline");
-
-    //Remove offset at the top
-    pagecontainer.css("marginTop", "20px");
-};
-
+    pageheader.innerHTML = "You are " + age + " years old";
+}
+refreshbtn.addEventListener("click", function () {
+    alert("Thank you. Your feedback has been sent to /dev/null");
+});
 function processImage(callback) {
-    var file = imgSelector.get(0).files[0]; //get(0) is required as imgSelector is a jQuery object so to get the DOM object, its the first item in the object. files[0] refers to the location of the photo we just chose.
-     var reader = new FileReader();
+    var file = imgSelector.files[0];
+    var reader = new FileReader();
     if (file) {
-        reader.readAsDataURL(file); //used to read the contents of the file
+        reader.readAsDataURL(file);
     }
     else {
         console.log("Invalid file");
     }
     reader.onloadend = function () {
-        //After loading the file it checks if extension is jpg or png and if it isnt it lets the user know.
         if (!file.name.match(/\.(jpg|jpeg|png)$/)) {
-            pageheader.innerHTML = "Only jpg and png images are accepted";
+            pageheader.innerHTML = "Please upload an image file (jpg or png).";
         }
         else {
-            //if file is photo it sends the file reference back up
             callback(file);
         }
     };
 }
-
-
-function changeUI() {
-    //Show detected mood
-    pageheader.innerHTML = "Your age is: " + age;  //Remember currentMood is a Mood object, which has a name and emoji linked to it. 
-    //Show mood emoji
- //   var img = document.getElementById("selected-img"); //getting a predefined area on our webpage to show the emoji
-  //  img.src = currentMood.emoji; //link that area to the emoji of our currentMood.
-  //  img.style.display = "inline"; //just some formating of the emoji's location
-
-}
-
 function sendEmotionRequest(file, callback) {
     var params = {
-            // Request parameters
-            "returnFaceId": "true",
-            "returnFaceLandmarks": "true",
-            "returnFaceAttributes": "age,gender",
-        };
+        // Request parameters
+        "returnFaceId": "true",
+        "returnFaceLandmarks": "true",
+        "returnFaceAttributes": "age,gender"
+    };
     $.ajax({
-         url: "https://api.projectoxford.ai/face/v1.0/detect?" + $.param(params),
-            beforeSend: function(xhrObj) {
+        url: "https://api.projectoxford.ai/face/v1.0/detect?" + $.param(params),
+        beforeSend: function (xhrObj) {
             // Request headers
             xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
             xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "dd2587080bf44ca69a5c240f2f9169b4");
@@ -73,38 +58,20 @@ function sendEmotionRequest(file, callback) {
         processData: false
     })
         .done(function (data) {
-            if (data.length != 0) { // if a face is detected
-                // Get the face attributes
-                var faceAttributes = data[0].faceAttributes;
-                callback(faceAttributes);
-            } else {
-                pageheader.innerHTML = "A human face was not detected. Please try uploading another photo. ";
-            }
-        })
+        if (data.length != 0) {
+            var faceAttributes = data[0].faceAttributes;
+            callback(faceAttributes);
+        }
+        else {
+            pageheader.innerHTML = "A human face was not detected. Please try uploading another photo. ";
+        }
+    })
         .fail(function (error) {
-            pageheader.innerHTML = "Sorry, something went wrong. :( Try again in a bit?";
-            console.log(error.getAllResponseHeaders());
-        });
+        pageheader.innerHTML = "An error occured, please try again later";
+        console.log(error.getAllResponseHeaders());
+    });
 }
-
 function getAge(faceAttributes) {
-    var age;
-    age = faceAttributes.age;
+    var age = faceAttributes.age;
     return age;
 }
-
-
-        // Register event listeners
-imgSelector.on("change", function () {
-    pageheader.innerHTML = "Analysing your face now."; //good to let your user know something is happening!
-    processImage(function (file) { //this checks the extension and file
-        // Get emotions based on image
-        sendEmotionRequest(file, function (faceAttributes) { //here we send the API request and get the response
-            // Find out most dominant emotion
-            age = getAge(faceAttributes);  //this is where we send out scores to find out the predominant emotion
-            changeUI(); //time to update the web app, with their emotion!
-
-            //Done!!
-        });
-    });
-});
